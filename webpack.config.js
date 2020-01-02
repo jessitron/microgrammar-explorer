69626c7741fd1b02a608b116a57eededb0133cbf
Jessica Kerr <jessitron@gmail.com>
2020-01-01 20:27:32 -0600

    Enable live reload of code changes
    Hooray webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'static')],
+  },
   context: path.join(__dirname, './'),
   entry: './app/app.tsx',
   output: {

87e611ea1b278e4116e249477aa320402b3685d1
Jessica Kerr <jessitron@gmail.com>
2020-02-15 18:24:58 -0600

    Initialize the tree

--- app/components/TreeParseGUI.tsx
+++ app/components/TreeParseGUI.tsx
@@ -33,7 +33,7 @@ export class TreeParseGUI extends React.Component<{},
   }
 
   public componentDidMount() {
-    // whatever.
+    this.updateTree();
   }
 
   public updateTree = _.debounce(async () => {
